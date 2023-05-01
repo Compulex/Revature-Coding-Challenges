d8b1e5899d1950bf94dd4b8a59b303e28b82b45c
Alexa Lewis <alexalewis2013@afscholars.org>
2023-05-01 17:11:02 -0400

    java docs

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 /**
  * 05-01-23
- * Create a function that takes numbers b and m as arguments and returns the derivative of the function
- * f(x)=x^b with respect to x evaluated at x=m, where b and m are constants.
- * Example
- *  derivative(1, 4) ➞ 1
- *  derivative(3, -2) ➞ 12
- *  derivative(4, -3) ➞ -108
+ * There's a great war between the even and odd numbers. Many numbers already lost their lives in this war, and it's
+ * your task to end this. You have to determine which group sums larger: the evens, or the odds. The larger group wins.
+ * <p>
+ * Create a function that takes an array of integers, sums the even and odd numbers separately, then returns the
+ * difference between the sum of the even and odd numbers.
+ * <p>
+ * Examples
+ * warOfNumbers([2, 8, 7, 5]) ➞ 2
+ * // 2 + 8 = 10
+ * // 7 + 5 = 12
+ * // 12 is larger than 10
+ * // So we return 12 - 10 = 2
+ * warOfNumbers([12, 90, 75]) ➞ 27
+ * warOfNumbers([5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243]) ➞ 168
  */
 
 public class Main {
